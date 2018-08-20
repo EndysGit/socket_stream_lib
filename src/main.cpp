d2d28c397fb7ffadb4c19a02662476ab76921117
EndysGit <endysmail@yandex.ru>
2018-08-21 01:45:30 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <ostream>
+#include <ios>
+
+class socket
+{
+};
+
+/* stream prefixes:
+ *  - i = input
+ *  - o = output
+ *  - s = socket
+ */
+
+template <typename charT, typename traits, typename socketT>
+class basic_isstreami : virtual public std::basic_ios<charT, traits>
+{
+};
+
+template <typename charT, typename traits, typename socketT>
+class basic_osstream : virtual public std::basic_ios<charT, traits> 
+{
+
+};
+
+int
+main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
